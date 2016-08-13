7c8bc2c31d524b0011fc0c9fccd0fb679089d48d
Mateus Pinheiro <mateusp@br.ibm.com>
2016-08-13 00:22:42 -0300

    Remove extraneous characters

--- lib/active_couch.ts
+++ lib/active_couch.ts
@@ -21,53 +21,53 @@ export default class ActiveCouch implements CouchDocument {
 	schema:  string;
 	doc:     CouchDocument;
 
- 	constructor(e: CouchDocument) {
- 		this._id  = e._id  || undefined;
- 		this._rev = e._rev || undefined;
+	constructor(e: CouchDocument) {
+		this._id  = e._id  || undefined;
+		this._rev = e._rev || undefined;
 
- 		this.doc = e;
- 	}
+		this.doc = e;
+	}
 
- 	save():Q.Promise<Object|joi.ValidationError> {
- 		let q = Q.defer();
- 		let isValid = this.isValid();
+	save():Q.Promise<Object|joi.ValidationError> {
+		let q = Q.defer();
+		let isValid = this.isValid();
 
- 		if(!isValid) {
- 			q.reject(isValid);
- 		} else {
+		if(!isValid) {
+			q.reject(isValid);
+		} else {
  			Db.insert(this.doc, {}, (error, result) => {
 	 			if (error) q.reject(error);
 	 			else q.resolve(result);
 	 		});
- 		}
+		}
 
- 		return q.promise;
- 	}
+		return q.promise;
+	}
 
- 	delete():Q.Promise<Object|Error> {
- 		let q = Q.defer();
+	delete():Q.Promise<Object|Error> {
+		let q = Q.defer();
 
- 		if(!this._id) {
- 			q.reject(new Error('This document has not yet been saved, thus can not be deleted'));
- 		} else if(!this._rev) {
- 			q.reject(new Error('This document does not have a revision, thus can not be deleted'));
- 		} else {
- 			Db.destroy(this._id, this._rev, (error, result) => {
- 				if (error) q.reject(error);
- 				else q.resolve(result);
- 			});
- 		}
+		if(!this._id) {
+			q.reject(new Error('This document has not yet been saved, thus can not be deleted'));
+		} else if(!this._rev) {
+			q.reject(new Error('This document does not have a revision, thus can not be deleted'));
+		} else {
+			Db.destroy(this._id, this._rev, (error, result) => {
+				if (error) q.reject(error);
+				else q.resolve(result);
+			});
+		}
 
- 		return q.promise;
- 	}
+		return q.promise;
+	}
 
- 	isValid():Boolean | joi.ValidationError {
- 		if(!this.schema) return false;
- 		if(!this.doc) return false;
+	isValid():Boolean | joi.ValidationError {
+		if(!this.schema) return false;
+		if(!this.doc) return false;
 
- 		joi.validate(this.doc, this.schema, (error, validatedObj) => {
- 			if(error) return error;
- 			else return true;
- 		});
- 	}
+		joi.validate(this.doc, this.schema, (error, validatedObj) => {
+			if(error) return error;
+			else return true;
+		});
+	}
 }
